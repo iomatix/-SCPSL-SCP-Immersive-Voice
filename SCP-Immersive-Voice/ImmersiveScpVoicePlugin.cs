a5b972c7461d56abbc04ffb4d91a891f425d05a1
iomatix <mateuszwypchlak@gmail.com>
2026-06-10 00:29:16 +0200

    1.0.1

--- SCP-Immersive-Voice/ImmersiveScpVoicePlugin.cs
+++ SCP-Immersive-Voice/ImmersiveScpVoicePlugin.cs
@@ -25,7 +25,7 @@
         public override string Name => "SCP Voice Chat";
         public override string Description => "Enables proximity voice chat for SCPs and adds audio effects";
         public override string Author => "iomatix";
-        public override Version Version => new Version(1,0,0);
+        public override Version Version => new Version(1,0,1);
         public override Version RequiredApiVersion { get; } = new Version(LabApiProperties.CompiledVersion);
         #endregion
 
